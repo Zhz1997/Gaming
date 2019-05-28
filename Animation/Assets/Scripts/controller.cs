4f42f1fed537e97e0a9b9216c71cc5bccef302b4
Zhz1997 <41028016+Zhz1997@users.noreply.github.com>
2019-05-28 01:33:31 -0700

    upload animation

---
+++ Animation/Assets/Scripts/controller.cs
@@ -0,0 +1,171 @@
+﻿/*
+UVic CSC 305, 2019 Spring
+
+Helping lab for assignment03
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+
+    public GameObject boid;
+    public GameObject marker;
+    int boidCount = 50;
+
+    private class Boids
+    {
+        public GameObject boidObject;
+        public Vector3 velocity = Random.insideUnitSphere*Random.Range(0.2f,0.3f);
+        public Vector3 acceleration = new Vector3 (0,0,0);
+        public float maxForce = 0.5f;
+        public float maxSpeed = 0.3f;
+    }
+    private List<Boids> boidList;
+
+    void Start()
+    {
+        //boidList = new List<GameObject>();
+        boidList = new List<Boids>();
+
+        for (int i = 0; i < boidCount; ++i)
+        {
+            GameObject newBoid = new GameObject();
+            newBoid.transform.parent = gameObject.transform;
+            newBoid.name = "boid No." + i.ToString();
+
+            GameObject instPrefab = Instantiate(boid);
+            instPrefab.transform.parent = newBoid.transform;
+
+            Boids boidd = new Boids();
+            boidd.boidObject = newBoid;
+            boidd.boidObject.transform.position = new Vector3(boid.transform.position.x + i+10, boid.transform.position.y, boid.transform.position.z+1+10);
+            boidList.Add(boidd);
+
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        foreach(var boid in boidList)
+        {
+            Vector3 allignment = Allign(boid)*1f;
+            Vector3 coh = Cohesion(boid) * 1f;
+            Vector3 sep = Seperation(boid) * 10f;
+
+
+            boid.acceleration = boid.acceleration + allignment + coh + sep;
+            boid.velocity = boid.velocity+boid.acceleration;
+            if (boid.velocity.magnitude>boid.maxSpeed)
+            {
+                boid.velocity = boid.velocity.normalized * boid.maxSpeed;
+            }
+            Vector3 directiona = (marker.transform.position - boid.boidObject.transform.GetChild(0).position).normalized*0.4f + boid.velocity;
+            //Vector3 directiona = boid.velocity;
+            boid.boidObject.transform.GetChild(0).position += directiona.normalized * boid.velocity.magnitude;
+            boid.boidObject.transform.GetChild(0).LookAt(marker.transform);
+
+            Quaternion toRotationa = Quaternion.FromToRotation(transform.up, directiona);
+            boid.boidObject.transform.GetChild(0).rotation = toRotationa;
+            boid.acceleration = new Vector3(0, 0, 0);
+        }
+
+    }
+
+    private Vector3 Allign(Boids boid)
+    {
+        float perceptionRadius = 30f;
+        Vector3 steering = new Vector3(0, 0, 0);
+        int total = 0;
+        foreach (var boidd in boidList)
+        {
+            float distance = Vector3.Distance(boid.boidObject.transform.GetChild(0).position,boidd.boidObject.transform.GetChild(0).position);
+            if(boidd != boid && distance < perceptionRadius)
+            {
+                steering = steering + boidd.velocity;
+                total = total + 1;
+            }
+        }
+
+        if (total>0)
+        {
+            steering = steering / total;
+            steering = steering.normalized * boid.maxSpeed;
+            steering = steering - boid.velocity;
+            if (steering.magnitude>boid.maxForce)
+            {
+                steering = steering.normalized * boid.maxForce;
+            }
+
+        }
+        return steering;
+    }
+
+    private Vector3 Cohesion(Boids boid)
+    {
+        float perceptionRadius = 100f;
+        Vector3 steering = new Vector3(0, 0, 0);
+        int total = 0;
+        foreach (var boidd in boidList)
+        {
+            float distance = Vector3.Distance(boid.boidObject.transform.GetChild(0).position, boidd.boidObject.transform.GetChild(0).position);
+            if (boidd != boid && distance < perceptionRadius)
+            {
+                steering = steering + boidd.boidObject.transform.GetChild(0).position;
+                total = total + 1;
+            }
+        }
+
+        if (total > 0)
+        {
+            steering = steering / total;
+            steering = steering - boid.boidObject.transform.GetChild(0).position;
+            steering = steering.normalized * boid.maxSpeed;
+            steering = steering - boid.velocity;
+            if (steering.magnitude > boid.maxForce)
+            {
+                steering = steering.normalized * boid.maxForce;
+            }
+
+        }
+        return steering;
+    }
+
+    private Vector3 Seperation(Boids boid)
+    {
+        float perceptionRadius = 20f;
+        Vector3 steering = new Vector3(0, 0, 0);
+        int total = 0;
+        foreach (var boidd in boidList)
+        {
+            float distance = Vector3.Distance(boid.boidObject.transform.GetChild(0).position, boidd.boidObject.transform.GetChild(0).position);
+            if (boidd != boid && distance < perceptionRadius)
+            {
+                Vector3 diff = boid.boidObject.transform.GetChild(0).position - boidd.boidObject.transform.GetChild(0).position;
+                diff = diff / (distance * distance);
+                steering = steering + diff;
+                total = total + 1;
+            }
+        }
+
+        if (total > 0)
+        {
+            steering = steering / total;
+            steering = steering.normalized * boid.maxSpeed;
+            steering = steering - boid.velocity;
+            if (steering.magnitude > boid.maxForce)
+            {
+                steering = steering.normalized * boid.maxForce;
+            }
+
+        }
+        return steering;
+    }
+
+
+}
---
+++ Animation/Assets/Scripts/follow.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform route;
+    private float tParam;
+
+    private Vector3 arrowPos;
+    private float speedModifier;
+    private Vector3 p0;
+    private Vector3 p1;
+    private Vector3 p2;
+    private Vector3 p3;
+    private List<Vector2> posL;
+    private List<Vector2> timeL;
+    private int count;
+    private float totalCurLen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalCurLen = 0f;
+        count = -1;
+        posL = new List<Vector2>();
+        timeL = new List<Vector2>();
+        tParam = 0f;
+        speedModifier = 0.1f;
+
+        p0 = route.GetChild(0).position;
+        p1 = route.GetChild(1).position;
+        p2 = route.GetChild(2).position;
+        p3 = route.GetChild(3).position;
+
+        for(float i = 0; i <= 1; i = i + 0.000005f)
+        {
+            Vector2 temp = new Vector2(i, GetLen(i, i + 0.000005f));
+            posL.Add(temp);
+            totalCurLen = totalCurLen + GetLen(i, i + 0.000005f);
+        }
+
+        float parCurLen = totalCurLen / 500;
+        float curLenCount = 0;
+
+        foreach (var i in posL)
+        {         
+            curLenCount = curLenCount + i.y;
+
+            if (curLenCount >= parCurLen)
+            {
+                Vector2 temp = new Vector2(i.x, curLenCount);
+                timeL.Add(temp);
+                curLenCount = 0;
+            }
+        }
+
+        foreach (var i in timeL)
+        {
+            Debug.Log(i.x.ToString() + " " + i.y.ToString());
+        }
+
+        Debug.Log("Length of posL" + posL.Count.ToString());
+        Debug.Log("Length of timeL" + timeL.Count.ToString());
+        Debug.Log("total curve length is" + totalCurLen.ToString());
+        Debug.Log("1/200 curve length is" + parCurLen.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count = count + 1;
+        if(count == timeL.Count)
+        {
+            count = 0;
+        }
+        Vector3 arrowPos = new Vector3();
+
+        tParam = timeL[count].x;
+
+        arrowPos = Mathf.Pow(1 - tParam, 3) * p0 +
+          3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 + 
+          3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 + 
+          Mathf.Pow(tParam, 3) * p3;
+
+    
+        
+        transform.position = arrowPos;
+    }
+
+    private float GetLen(float prev,float next)
+    {
+        Vector3 prevPos = Mathf.Pow(1 - prev, 3) * p0 +
+            3 * Mathf.Pow(1 - prev, 2) * prev * p1 +
+            3 * (1 - prev) * Mathf.Pow(prev, 2) * p2 +
+            Mathf.Pow(prev, 3) * p3;
+
+        Vector3 nextPos = Mathf.Pow(1 - next, 3) * p0 +
+            3 * Mathf.Pow(1 - next, 2) * next * p1 +
+            3 * (1 - next) * Mathf.Pow(next, 2) * p2 +
+            Mathf.Pow(next, 3) * p3;
+
+        return Vector3.Distance(nextPos, prevPos);
+    }
+
+
+  
+}
